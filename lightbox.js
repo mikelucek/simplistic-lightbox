012fe73ae776f9bbf302e0923a68c82aa2502943
Mike Lucek <michael.s.lucek@gmail.com>
2015-08-27 17:01:11 -0400

    Format Cleanup

--- lightbox.js
+++ lightbox.js
@@ -1,20 +1,28 @@
 $(document).ready(function(){
 	$(".lightbox-link").click(function(e){
 		
-		
+		var link_element = $(e.currentTarget);
+		var imageURL = link_element.data("img-url");
 
-		$link_element = $(e.currentTarget);
-		var image = $link_element.data("img-url");
-
-		$('#lb-image').attr('src', image);
-
+		var image = $("#lightbox-image");
+		image.attr('src', imageURL);
+		centerImage(image);
 
 		$('.lightbox-outer').show();
 
-		$(".lightbox-outer, .lightbox-inner, .lightbox-inner img").click(function(){
-					
-				$(".lightbox-outer").hide();
+		$(".lightbox-outer").click(function(){		
+			$(".lightbox-outer").hide();
 		});
 
 	});
 });
+
+function centerImage(image) {
+
+	image.load(function(){
+		var page_height = $(window).height();
+		var image_height = image.height();
+		var image_height_offset = ( page_height - image_height ) / 2;
+		image.parent().css("margin-top", image_height_offset);
+	});
+}
